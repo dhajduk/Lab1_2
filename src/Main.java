e6e87d9986781ed73871de0a152f36f9e4cf7f8d
dhajduk <gato123@poczta.fm>
2023-11-18 22:44:06 +0100

    Zad.3

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+class Hello
+{
+    static String name = "Alan";
+    static int age = 41;
+    static int hight = 180;
+    public static void main(String[] args)
+    {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Hight: " + hight);
+    }
+}
